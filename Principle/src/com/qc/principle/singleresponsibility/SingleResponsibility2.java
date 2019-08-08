a7952e347e4297e1d79857cad357575e75bf3b98
cxy1686901657 <1686901657@qq.com>
2019-08-08 11:26:14 +0800

    单一职责

---
+++ Principle/src/com/qc/principle/singleresponsibility/SingleResponsibility2.java
@@ -0,0 +1,47 @@
+package com.qc.principle.singleresponsibility;
+
+/**
+ * @author qc
+ * @date 2019/8/8
+ * @description 单一职责
+ * @model 设计原则
+ * @project JavaDesignMode
+ */
+
+public class SingleResponsibility2 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        RoadVehicle roadVehicle = new RoadVehicle();
+        roadVehicle.run("摩托车");
+        roadVehicle.run("汽车");
+
+        AirVehicle airVehicle = new AirVehicle();
+
+        airVehicle.run("飞机");
+    }
+
+}
+
+//方案2的分析
+//1. 遵守单一职责原则
+//2. 但是这样做的改动很大，即将类分解，同时修改客户端
+//3. 改进：直接修改Vehicle 类，改动的代码会比较少=>方案3
+
+class RoadVehicle {
+    public void run(String vehicle) {
+        System.out.println(vehicle + "公路运行");
+    }
+}
+
+class AirVehicle {
+    public void run(String vehicle) {
+        System.out.println(vehicle + "天空运行");
+    }
+}
+
+class WaterVehicle {
+    public void run(String vehicle) {
+        System.out.println(vehicle + "水中运行");
+    }
+}
