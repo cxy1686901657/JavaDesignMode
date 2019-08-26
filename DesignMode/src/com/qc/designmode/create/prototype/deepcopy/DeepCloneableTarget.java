c932c4e2c79c999ca9e3f8948c957e04e2c93863
cxy1686901657 <1686901657@qq.com>
2019-08-26 16:13:57 +0800

    深拷贝（Clone）

---
+++ DesignMode/src/com/qc/designmode/create/prototype/deepcopy/DeepCloneableTarget.java
@@ -0,0 +1,28 @@
+package com.qc.designmode.create.prototype.deepcopy;
+
+import java.io.Serializable;
+
+/**
+ * 深拷贝
+ */
+public class DeepCloneableTarget implements Serializable, Cloneable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String cloneName;
+
+	private String cloneClass;
+
+	public DeepCloneableTarget(String cloneName, String cloneClass) {
+		this.cloneName = cloneName;
+		this.cloneClass = cloneClass;
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+}
